d19409ca3ffe4686ada8c550382a6511430b8109
Zeidash <adam.zajicek.020@pslib.cz>
2021-11-23 00:13:04 +0100

    Carousel is absolutely insane

--- main.js
+++ main.js
@@ -19,7 +19,25 @@ function effect(e) {
 
 //Carousel
 
+
+
+let jsondata2 = '{ "champions" : [' +
+'{ "name":"Ashe" , "text": "Iceborn warmother of the Avarosan tribe, Ashe commands the most populous horde in the north. Stoic, intelligent, and idealistic, yet uncomfortable with her role as leader, she taps into the ancestral magics of her lineage to wield a bow of True Ice..." },' +
+'{ "name":"Samira" , "text": "Samira stares death in the eye with unyielding confidence, seeking thrill wherever she goes. After her Shuriman home was destroyed as a child, Samira found her true calling in Noxus, where she built a reputation as a stylish daredevil taking on..." },' +
+'{ "name":"Miss Fortune" , "text": "A Bilgewater captain famed for her looks but feared for her ruthlessness, Sarah Fortune paints a stark figure among the hardened criminals of the port city. As a child, she witnessed the reaver king Gangplank murder her family..." },' +
+'{ "name":"Xayah" , "text": "Deadly and precise, Xayah is a vastayan revolutionary waging a personal war to save her people. She uses her speed, guile, and razor-sharp feather blades to cut down anyone who stands in her way. Xayah fights alongside her partner and lover, Rakan..." },' +
+'{ "name":"KaiSa" ,  "text": "To survive among predators, Kai’Sa became one. Stranded for years with no hope of rescue, Kai’Sa endured through sheer will and a symbiotic second skin adapted from a living Void creature..." },' +
+'{ "name":"Jhin" , "text": "Jhin is a meticulous criminal psychopath who believes murder is art. Once an Ionian prisoner, but freed by shadowy elements within Ionia’s ruling council, the serial killer now works as their cabal\'s assassin. Using his gun as his paintbrush..." } ]}'; 
+
+const obj = JSON.parse(jsondata2);
+
 let count = 0; //Add or subtract '1' when pressing buttons
+let heading = document.getElementById("champ-name");
+let descriptionP = document.getElementById("champ-description");
+
+heading.innerText = obj.champions[count].name;
+descriptionP.innerText = obj.champions[count].text
+
 let img = [];
 img = document.getElementsByClassName("img"); //Array of all images
 const buttonPrev = document.getElementById("prev");
@@ -27,7 +45,7 @@ const buttonNext = document.getElementById("next");
 
 function moveRight() {
     if (count < 5) {
-        console.log(img[0])
+        console.log(img[0]);
         img[0].style.transform += "translate(-500px)";
         img[1].style.transform += "translate(-500px)";
         img[2].style.transform += "translate(-500px)";
@@ -35,8 +53,7 @@ function moveRight() {
         img[4].style.transform += "translate(-500px)";
         img[5].style.transform += "translate(-500px)";
         count++;
-    }
-    else {
+    } else {
         count = 0;
         img[0].style.transform = "translate(0px)";
         img[1].style.transform = "translate(0px)";
@@ -45,6 +62,7 @@ function moveRight() {
         img[4].style.transform = "translate(0px)";
         img[5].style.transform = "translate(0px)";
     }
+
     checkActive();
 }
 
@@ -57,8 +75,7 @@ function moveLeft() {
         img[4].style.transform = "translate(-2500px)";
         img[5].style.transform = "translate(-2500px)";
         count = 5;
-    }
-    else {
+    } else {
         img[0].style.transform += "translate(+500px)";
         img[1].style.transform += "translate(+500px)";
         img[2].style.transform += "translate(+500px)";
@@ -69,7 +86,8 @@ function moveLeft() {
     }
     checkActive();
 }
-function checkActive(){
+
+function checkActive() {
     img[0].style.opacity = "0";
     img[1].style.opacity = "0";
     img[2].style.opacity = "0";
@@ -78,4 +96,7 @@ function checkActive(){
     img[5].style.opacity = "0";
 
     img[count].style.opacity = "1";
+
+    heading.innerText = obj.champions[count].name;
+    descriptionP.innerText = obj.champions[count].text
 }
